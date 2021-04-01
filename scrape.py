958b3ea149ef08f066db09d07572bd2446e6da06
extrasauceontop <msuchemba@gmail.com>
2021-04-01 14:08:05 -0400

    scrape

--- scrape.py
+++ scrape.py
@@ -46,7 +46,7 @@ def fetch_data():
     ]
     for url in urls:
         r = session.get(url, headers=headers)
-        print(r)
+        # print(r)
         for item in json.loads(r.content):
             store = item["storeId"]
             name = item["name"]
